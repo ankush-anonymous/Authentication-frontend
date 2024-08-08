05939678feab9415703ddf1812b091789074d46f
ankush-anonymous <canankushdoit@gmai.com>
2024-08-08 20:28:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import SignIn from "./Pages/SignInPage";
+import SignIn from "./Pages/SignInPage";
+import TestPage from "./Pages/TestPage";
+import SignUp from "./Pages/SignUpPage";
+import DashboardPage from "./Pages/DashboardPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ToastContainer /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/DashboardPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LayoutComponent from "../Components/LayoutComponent"; // Adjust the path as necessary
+
+const DashboardPage = () => {
+  return (
+    <LayoutComponent>
+      {/* Your Dashboard content goes here */}
+      <h1>Welcome to the Dashboard</h1>
+    </LayoutComponent>
+  );
+};
+
+export default DashboardPage;
